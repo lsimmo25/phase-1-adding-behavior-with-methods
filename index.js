fff29084b06939751c99576909739d161dc7dc58
Lyle Simmons <lylesimmons125@gmail.com>
2024-02-18 11:37:14 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Cat {
+    constructor (name) {
+        //constructor data here
+        this.name = name;
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor (name) {
+        //constructor data here
+        this.name = name;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor (name, gender) {
+        //constructor data here
+        this.name = name;
+        this.gender = gender;
+    }
+    speak() {
+        if (this.gender === 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
